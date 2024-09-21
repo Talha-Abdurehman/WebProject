2f973ea108ea36e1e1829cee40f594c5bfdb7437
Talha-Abdurehman <talhakyle124@gmail.com>
2024-09-21 06:21:29 +0500

    WTF

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // main.js
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
 
 // Function to add a user from the form
